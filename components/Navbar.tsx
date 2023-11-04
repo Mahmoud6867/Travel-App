f3af584e00dc181e89142a4cb6745ae55b1605c5
Mahmoud <mahmoudalaa6868@gmail.com>
2023-11-04 23:08:57 +0200

    finished navbar and stoped video at 00:31:48

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,20 +9,32 @@ const Navbar = () => {
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="Logo" width={74} height={29} />
       </Link>
-        <ul className="hidden h-full gap-12 lg:flex">
-          {NAV_LINKS.map((link) => (
-            <Link
-              className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
-              href={link.href}
-              key={link.key}
-            >
-              {link.label}
-            </Link>
-          ))}
-        </ul>
-        <div className="lg:flexCenter hidden">
-          <Button type='button' title='Login' icon='/user.svg' variant = 'btn_dark_green'/>
-        </div>
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+            href={link.href}
+            key={link.key}
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+      <div className="lg:flexCenter hidden">
+        <Button
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
+      <Image
+        src="menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className=" inline-block cursor-pointer lg:hidden"
+      />
     </nav>
   );
 };
