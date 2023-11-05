d6db83fa4ebc89b2d1c2e21ac4ccd494707220c1
Mahmoud <mahmoudalaa6868@gmail.com>
2023-11-05 14:47:17 +0200

    stoped in video at

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-import Image from 'next/image'
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Guid from "@/components/Guid";
+import Hero from "@/components/Hero";
+import Image from "next/image";
 
 export default function Home() {
-  return (
-    <h1 className='font-bold text-teal-500 text-2xl'>hello world</h1>
-  )
+  return <>
+  <Hero/>
+  <Camp/>
+  <Guid/>
+  <Features/>
+  <GetApp/>
+  </>;
 }
